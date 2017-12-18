b92a8daf43c7614ea59bdf1a34f4b50632acff09
huangpengling <960606424@qq.com>
2017-12-18 17:43:23 +0800

    提交新代码

---
+++ list1_tup_dict.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# user_name = input("请输入你的名字:")
+# print(user_name)
+# print(type(user_name))
+
+# user_age = input("请输入你的年龄:")
+# print("你的年龄是:%s"%(user_age))
+# print(type(user_age))
+
+str = "huangpengling"
+print(str[0:4])
