0d9fbd2f059a757e440e9cdd5b94398242ba1968
huangpengling <960606424@qq.com>
2017-12-19 00:09:59 +0800

    提交代码

--- list1_tup_dict.py
+++ list1_tup_dict.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python
 
-# user_name = input("请输入你的名字:")
-# print(user_name)
-# print(type(user_name))
 
-# user_age = input("请输入你的年龄:")
-# print("你的年龄是:%s"%(user_age))
-# print(type(user_age))
+list1 = ["Google","IE",333,0.23,"我们"]
+#
+# for lists in list1:
+#     print(lists)
 
-str = "huangpengling"
-print(str[0:4])
+
+
+list2 = ["Google","IE",333,0.23,"我们"]
+# print(list2[0])
+# print(list2[0:3])
+# print(list2[:2])
+# print(list2[0:3])
+
+print(list2)
+list2.insert(0,'lam')
+print(list2)
+
+print(list1)
+list1.pop()
+print(list1)
+
+print(list1)
+list1.pop(2)
+print(list1)
+
+##合并列表
+list2 = ["Google","IE",333,0.23,"我们"]
+list1 = ["apple", "orange", "pear", "banana"]
+
+for i in list1:
+    list2.append(i)
+print(list2)
