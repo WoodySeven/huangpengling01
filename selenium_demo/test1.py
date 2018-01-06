280f975a7bd4c66fbddd1e8f7a61b20c5ccc38ce
huangpengling <960606424@qq.com>
2018-01-06 13:03:59 +0800

    更新代码

--- selenium_demo/test1.py
+++ selenium_demo/test1.py
@@ -8,13 +8,18 @@ class Login(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(5)
-        self.base_url = "file:///E:/automated_testing/github/huangpengling01/selenium_demo/test.html"
+        ##读配置文件打开本地文件
+        cf = configparser.ConfigParser()
+        cf.read("test.conf")
+        self.base_url = str(cf.get("file", "file_path"))
+        ##直接打开文件路径
+        # self.base_url = "file:///E:/automated_testing/github/huangpengling01/selenium_demo/test.html"
 
     def test_bugfree_login(self):
         self.browser.get(self.base_url)
         for i in range(6):
             if i %2 == 1:
-                element = 'label:nth-child({x}) input'.format(x=i)
+                element = '.table:nth-child({x}) input'.format(x=i)
                 self.browser.find_element_by_css_selector(element).click()
                 time.sleep(2)
 
