347ff4953567c43485b784c6a07a423b7327edfc
huangpengling <960606424@qq.com>
2018-01-06 12:51:13 +0800

    更新代码

--- selenium_demo/test1.py
+++ selenium_demo/test1.py
@@ -2,13 +2,13 @@
 import unittest
 import time
 from selenium import webdriver
-
+import configparser
 
 class Login(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(5)
-        self.base_url = "file:///E:/自动化测试/github/huangpengling01/selenium _demo/test.html"
+        self.base_url = "file:///E:/automated_testing/github/huangpengling01/selenium_demo/test.html"
 
     def test_bugfree_login(self):
         self.browser.get(self.base_url)
